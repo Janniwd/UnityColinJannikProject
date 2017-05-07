69756503a0135ae3d942b0aa40ce0e2246a9f0a2
Janniwd <jannik.wurst@web.de>
2017-05-07 15:26:51 +0200

    Ini
    asdasd

---
+++ CJTowerDefence/TowerDefence/Assets/LevelMangagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMangagerScript : MonoBehaviour {
+    
+    public static Transform[] WayPoints;
+
+    void Awake()
+    {
+        WayPoints = new Transform[transform.childCount];
+        for (int i = 0; i < WayPoints.Length; i++)
+        {
+            WayPoints[i] = transform.GetChild(i).transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ CJTowerDefence/TowerDefence/Assets/Scrips/EnemyScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float speed = 1000f;
+    private Transform WayPointTransform;
+    private int WayPointIndex = 0;
+
+    void Start()
+    {
+        WayPointTransform = LevelMangagerScript.WayPoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 ToMove = WayPointTransform.position - transform.position;
+        transform.Translate(ToMove.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, WayPointTransform.position) <= 0.2f)
+        {
+            GetNewWayPoint();
+        }
+    }
+
+    void GetNewWayPoint()
+    {
+        if (WayPointIndex == LevelMangagerScript.WayPoints.Length - 1)
+        {
+            Debug.Log("Finish");
+        }
+        WayPointIndex++;
+        WayPointTransform = LevelMangagerScript.WayPoints[WayPointIndex];
+    }
+}
